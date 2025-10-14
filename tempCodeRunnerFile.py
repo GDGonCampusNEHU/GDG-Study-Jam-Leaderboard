ae6994fcd6eec47fedef93027bb5930a6fde389f
Soumojit Bhuin <soumojitbhuin2004@gmail.com>
2025-10-14 12:12:37 +0530

    Remove tempCodeRunnerFile.py from tracking

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-    for lab in LABS:
-        lab_quoted = f'"{lab}"'
-        response = supabase.table("participants").select("*", count="exact").eq(lab_quoted, "Yes").execute()
-        lab_counts[lab] = response.count
